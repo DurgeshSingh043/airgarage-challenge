36feff12bf4ac8574d0e817230b6fbf5f7e909f8
Durgesh Singh <45793814+DurgeshSingh043@users.noreply.github.com>
2021-07-11 15:43:35 +0530

    Implemented load more functionality

--- src/apis/searchAPIs.js
+++ src/apis/searchAPIs.js
@@ -3,16 +3,31 @@ import { SEARCH_API_BASE_URL } from '../constants';
 import {
   failedLoadingSearchData,
   loadingSearchData,
+  successfulyLoadedMoreSearchData,
   successfulyLoadedSearchData,
 } from '../redux/actions/searchActions';
 
-export const getSearchResults = async (dispatch, seartText) => {
+const _getSearchResult = async (seartText, limit) =>
+  await axios.get(
+    `${SEARCH_API_BASE_URL}/getparkingbylocation?location=${seartText}&limit=${limit}`,
+  );
+
+export const getSearchResults = async (dispatch, seartText, limit = 10) => {
   dispatch(loadingSearchData());
   try {
-    const result = await axios.get(
-      `${SEARCH_API_BASE_URL}/getparkingbylocation?location=${seartText}`,
-    );
-    dispatch(successfulyLoadedSearchData(result?.data));
+    const result = await _getSearchResult(seartText, limit);
+    dispatch(successfulyLoadedSearchData(result?.data, seartText));
+  } catch (err) {
+    console.error('Something went wrong due to ', err);
+    dispatch(failedLoadingSearchData('Something went wrong'));
+    return Promise.reject();
+  }
+};
+
+export const loadMoreResults = async (dispatch, seartText, limit) => {
+  try {
+    const result = await _getSearchResult(seartText, limit);
+    dispatch(successfulyLoadedMoreSearchData(result?.data));
   } catch (err) {
     console.error('Something went wrong due to ', err);
     dispatch(failedLoadingSearchData('Something went wrong'));
--- src/components/molecules/Breadcrum/index.js
+++
@@ -1,3 +0,0 @@
-import Breadcrum from './Breadcrum';
-
-export default Breadcrum;
