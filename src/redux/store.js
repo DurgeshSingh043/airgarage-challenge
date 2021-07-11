513946fa7ef374a5dffddba5930453eba7322fde
Durgesh Singh <45793814+DurgeshSingh043@users.noreply.github.com>
2021-07-11 22:18:14 +0530

    last

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger'; // for development only
+// import logger from 'redux-logger'; // for development only
 
 import rootReducer from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk, logger)),
+  applyMiddleware(thunk),
+  // composeEnhancers(applyMiddleware(thunk, logger)),
 );
